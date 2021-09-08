13ab5d37640ffecfde3083b194ecbfff921ebe59
ChaosReadman <pigment@mac.com>
2021-09-08 07:20:43 +0000

    DXF変換まで

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module gocvtest
 go 1.17
 
 require gocv.io/x/gocv v0.28.0
+
+require github.com/yofu/dxf v0.0.0-20190710012328-5a6d1e83f16c // indirect
--- gocvtest.go
+++ gocvtest.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 	"os"
 
+	"github.com/yofu/dxf"
 	"gocv.io/x/gocv"
 )
 
@@ -22,6 +23,28 @@ func readImg(file string) gocv.Mat {
 		os.Exit(1)
 	}
 	return img
+}
+
+func DrawContourToDXF(name string, c gocv.PointsVector) {
+	d := dxf.NewDrawing()
+	d.Header().LtScale = 100.0
+	d.AddLayer("flat", dxf.DefaultColor, dxf.DefaultLineType, true)
+	d.ChangeLayer("flat")
+
+	for i := 0; i < c.Size(); i++ {
+		for j := 0; j < c.At(i).Size()-1; j++ {
+			x1 := float64(c.At(i).At(j).X)
+			y1 := float64(c.At(i).At(j).Y)
+			x2 := float64(c.At(i).At(j + 1).X)
+			y2 := float64(c.At(i).At(j + 1).Y)
+			d.Line(x1, -y1, 0, x2, -y2, 0)
+		}
+	}
+
+	err := d.SaveAs(name)
+	if err != nil {
+		return
+	}
 }
 
 func main() {
@@ -45,4 +68,7 @@ func main() {
 	gocv.DrawContours(&img, contours, -1, color.RGBA{255, 0, 0, 0}, 3)
 	// ここまでを保存
 	writeImg("Contour.jpg", &img)
+
+	// DXFで出力
+	DrawContourToDXF("test.dxf", contours)
 }
