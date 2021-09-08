add51646be2ae2d9ec34f49144b7e5a34a53e9b9
ChaosReadman <pigment@mac.com>
2021-09-08 01:35:55 +0000

    少し整理

--- gocvtest.go
+++ gocvtest.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
-	"gocv.io/x/gocv"
 	"image/color"
 	"os"
+
+	"gocv.io/x/gocv"
 )
+
+func writeImg(file string, img *gocv.Mat) {
+	if ok := gocv.IMWrite(file, *img); !ok {
+		fmt.Printf("Failed to write image: %s\n", file)
+		os.Exit(1)
+	}
+}
 
 func main() {
 	// ファイル読み込み
@@ -19,27 +27,17 @@ func main() {
 	imgGray := gocv.NewMat()
 	gocv.CvtColor(img, &imgGray, gocv.ColorBGRToGray)
 	// ここまでを保存
-	grayPath := "gray.jpeg"
-	if ok := gocv.IMWrite(grayPath, imgGray); !ok {
-		fmt.Printf("Failed to write image: %s\n")
-		os.Exit(1)
-	}
+	writeImg("gray.jpg", &imgGray)
+
 	imgTreshold := gocv.NewMat()
 	gocv.Threshold(imgGray, &imgTreshold, 127, 255, 0)
 	// ここまでを保存
-	thresholdPath := "Threshold.jpeg"
-	if ok := gocv.IMWrite(thresholdPath, imgTreshold); !ok {
-		fmt.Printf("Failed to write image: %s\n")
-		os.Exit(1)
-	}
+	writeImg("Threshold.jpg", &imgTreshold)
+
 	// 輪郭を抽出
 	contours := gocv.FindContours(imgTreshold, gocv.RetrievalTree, gocv.ChainApproxSimple)
 	// 元のイメージに書き込む
 	gocv.DrawContours(&img, contours, -1, color.RGBA{255, 0, 0, 0}, 3)
 	// ここまでを保存
-	contourdPath := "Contour.jpeg"
-	if ok := gocv.IMWrite(contourdPath, img); !ok {
-		fmt.Printf("Failed to write image: %s\n")
-		os.Exit(1)
-	}
+	writeImg("Contour.jpg", &img)
 }
